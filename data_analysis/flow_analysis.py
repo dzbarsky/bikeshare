731a9ee71dd3f997727e29e37dff3c3cafcfab05
David Zbarsky <dzbarsky@gmail.com>
2014-11-08 18:20:42 -0500

    Add rebalancing output

--- data_analysis/flow_analysis.py
+++ data_analysis/flow_analysis.py
@@ -10,7 +10,10 @@ class CSVParser:
     def __init__(self, filename):
         print 'reading file'
         self.historical_counts = dict()
+        self.historical_rebalances = dict()
         self.station_counts = dict()
+        self.bike_locations = dict()
+        self.rebalances = dict()
 
         lines = pandas.read_csv(args.filename)
         self.current_date = time.strptime(lines.loc[0]['starttime'], '%Y-%m-%d %H:%M:%S')
@@ -19,7 +22,10 @@ class CSVParser:
             self.process_line(lines.loc[i])
 
         f = open(filename + '.json', 'w')
-        f.write(json.dumps(self.historical_counts))
+        o = dict()
+        o['historical_counts'] = self.historical_counts
+        o['historical_rebalances'] = self.historical_rebalances
+        f.write(json.dumps(o))
 
         print 'read file'
 
@@ -35,10 +41,32 @@ class CSVParser:
                 if k not in self.historical_counts:
                     self.historical_counts[k] = list()
                 self.historical_counts[k].append(v)
+
+            for (k, v) in self.rebalances.iteritems():
+                if k not in self.historical_rebalances:
+                    self.historical_rebalances[k] = list()
+                self.historical_rebalances[k].append(v)
+
             self.station_counts = dict()
+            self.rebalances = dict()
 
         start_station = line['start station name']
         end_station = line['end station name']
+        bikeid = line['bikeid']
+        if bikeid in self.bike_locations:
+            prev_station = self.bike_locations[bikeid]
+        else:
+            prev_station = None
+
+        self.bike_locations[bikeid] = end_station
+
+        if prev_station and start_station is not prev_station:
+            key = prev_station + "?" + start_station
+            if key not in self.rebalances:
+                self.rebalances[key] = 1
+            else:
+                self.rebalances[key] += 1
+
         if start_station not in self.station_counts:
             self.station_counts[start_station] = 0
         if end_station not in self.station_counts:
@@ -50,7 +78,10 @@ class DataParser:
 
     def __init__(self, filename):
         f = open(filename)
-        self.historical_counts = json.loads(f.read())
+
+        o = json.loads(f.read())
+        self.historical_counts = o['historical_counts']
+        self.historical_rebalances = o['historical_rebalances']
 
     def analyze(self):
         for (k, v) in self.historical_counts.iteritems():
