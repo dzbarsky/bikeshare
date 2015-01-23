2f7eddee4477a9fa3f9b47a5de98a23994788300
David Zbarsky <dzbarsky@gmail.com>
2015-01-23 13:30:54 -0500

    app changes

--- BikeShare/BikeShare/Views/StationMarker.swift
+++ BikeShare/BikeShare/Views/StationMarker.swift
@@ -15,7 +15,7 @@ class StationMarker: GMSMarker {
     super.init()
     
     position = station.coordinate
-    icon = UIImage(named: "bar_pin")
+    icon = UIImage(named: "rsz_1bike")
     groundAnchor = CGPoint(x: 0.5, y: 1)
     appearAnimation = kGMSMarkerAnimationPop
   }
