7e11fd56e4399b629d5184de3a8ec51a33615525
David Zbarsky <dzbarsky@gmail.com>
2015-02-28 14:44:47 -0500

    add option to return bikes

--- BikeShare/BikeShare/Controllers/MapViewController.swift
+++ BikeShare/BikeShare/Controllers/MapViewController.swift
@@ -5,6 +5,8 @@ import Darwin
 class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate, AVCaptureMetadataOutputObjectsDelegate {
   @IBOutlet weak var mapView: GMSMapView!
   @IBOutlet weak var pinImageVerticalConstraint: NSLayoutConstraint!
+  @IBOutlet weak var returnBike: UIButton!
+  @IBOutlet weak var elapsedTime: UILabel!
 
   let locationManager = CLLocationManager()
   let net = Net()
@@ -13,9 +15,13 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
   var previewLayer = CALayer()
   var reservationExpiration = NSTimeInterval()
   var timer = NSTimer()
-  var label = UILabel()
+  var timerLabel = UILabel(frame: CGRectMake(0, 0, 200, 21))
+  var currentBikeId = ""
+  var closestStationId = ""
   
   override func viewDidLoad() {
+    self.returnBike.hidden = true
+    self.elapsedTime.hidden = true
     super.viewDidLoad()
     locationManager.delegate = self
     mapView.delegate = self
@@ -37,12 +43,47 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
     previewLayer.hidden = true
     
     self.view.layer.addSublayer(previewLayer)
+    
+    timerLabel.center = CGPointMake(160, 284)
+    timerLabel.textColor = UIColor.redColor()
+    timerLabel.textAlignment = NSTextAlignment.Center
+    timerLabel.text = "Scan the bike's QR code"
+    timerLabel.hidden = true
+    self.view.addSubview(timerLabel)
+    
+    returnBike.addTarget(self, action: "returnBikeInUse", forControlEvents: .TouchUpInside)
+    
     session.startRunning()
   }
   
+  func returnBikeInUse() {
+    
+    self.returnBike.hidden = true
+    //self.elapsedTime.hidden = true
+    
+    let url = "http://sd-bikeshare.herokuapp.com/bikes/\(self.currentBikeId)/return?station=\(self.closestStationId)"
+    net.POST(url, params: [:],
+      successHandler: { responseData in
+        println("returned bike")
+        let result = responseData.json(error: nil)! as NSDictionary
+        
+        let time = result["Time elapsed"]! as Double
+        let alert = UIAlertController(title: "Bike Returned!", message: "Time elapsed: \(self.format(time))", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+        println()
+       
+      },
+      
+      failureHandler: { error in
+        println("error")
+        NSLog("Error")
+      }
+    )
+  }
+
   func fetchStations(coordinate: CLLocationCoordinate2D) {
     
-    println("fetching")
     mapView.clear()
 
     let url = NSURL(string: "http://sd-bikeshare.herokuapp.com/stations")
@@ -74,6 +115,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
         let insets = UIEdgeInsetsMake(80, 80, 80, 80)
         let camera = self.mapView.cameraForBounds(bounds, insets: insets)
         self.mapView.animateToCameraPosition(camera)
+        self.closestStationId = marker.station.id
       }
     }
     task.resume()
@@ -87,39 +129,34 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
       previewLayer.hidden = true
       isUnlockingBike = false
       timer.invalidate()
-      label.removeFromSuperview()
-      var alert = UIAlertController(title: "Reservation Expired!", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+      timerLabel.hidden = true
+      let alert = UIAlertController(title: "Reservation Expired!", message: "", preferredStyle: UIAlertControllerStyle.Alert)
       alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
       self.presentViewController(alert, animated: true, completion: nil)
     }
     
-    let seconds = Int(remaining / 1000) % 60
-    let minutes = Int(remaining / 60000)
-    
     dispatch_async(dispatch_get_main_queue()) {
-      self.label.text = String(format:"%02d:%02d", minutes, seconds)
+      self.timerLabel.text = self.format(remaining)
     }
+  }
+  
+  func format(time: Double) -> String {
+    let seconds = Int(time / 1000) % 60
+    let minutes = Int(time / 60000)
+    
+    return String(format:"%02d:%02d", minutes, seconds)
   }
   
   func mapView(mapView: GMSMapView!, didTapInfoWindowOfMarker marker: GMSMarker!) {
     let stationMarker = mapView.selectedMarker as StationMarker
-    println(stationMarker)
     if !stationMarker.hasBikes() {
       return
     }
-    
-    let url = "http://sd-bikeshare.herokuapp.com/stations/\(stationMarker.station.id)/reserve?user=\(userId)"
-    println(url)
-    
-    label = UILabel(frame: CGRectMake(0, 0, 200, 21))
-    label.center = CGPointMake(160, 284)
-    label.textColor = UIColor.redColor()
-    label.textAlignment = NSTextAlignment.Center
-    label.text = "I'm a test label"
-    self.view.addSubview(label)
 
+    self.timerLabel.hidden = false
     previewLayer.hidden = false
     
+    let url = "http://sd-bikeshare.herokuapp.com/stations/\(stationMarker.station.id)/reserve?user=\(userId)"
     net.POST(url, params: [:],
       successHandler: { responseData in
         println("post success handler")
@@ -140,18 +177,25 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
   }
   
   func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
-    println("capturing output")
     if isUnlockingBike {
       return
     }
     for item in metadataObjects {
       if let metadataObject = item as? AVMetadataMachineReadableCodeObject {
         if metadataObject.type == AVMetadataObjectTypeQRCode {
-          previewLayer.hidden = true
-          let url = "http://sd-bikeshare.herokuapp.com/bikes/\(metadataObject.stringValue)/unlock?user=\(userId)"
+          
           isUnlockingBike = true
+          previewLayer.hidden = true
+          timerLabel.hidden = true
+          returnBike.hidden = false
+          //elapsedTime.hidden = false
+          
+          let url = "http://sd-bikeshare.herokuapp.com/bikes/\(metadataObject.stringValue)/unlock?user=\(userId)"
           net.POST(url, params: [:], successHandler: { responseData in
             let result = responseData.json(error: nil)
+          
+            self.currentBikeId = result?["bike"] as String
+            
             NSLog("result: \(result)")
             }, failureHandler: { error in
               self.isUnlockingBike = true
