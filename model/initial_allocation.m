945112dfc6ab91ff2f7a76d0831521e2781f08ec
David Zbarsky <dzbarsky@gmail.com>
2015-03-20 16:16:29 -0400

    More iterations and store best to file

--- model/initial_allocation.m
+++ model/initial_allocation.m
@@ -10,51 +10,56 @@ function initial_allocation
             P(hour + 1, :, :) = dlmread(TRANSITIONS_FILENAME, '', range);
         end
 
+    
+    best_error = 999999999;
+    
+    for outer_iter = 1:5
         
-    NUM_ITERS = 100;
-    
-    best_errors = zeros(NUM_ITERS, 1);
-    best_es = zeros(NUM_ITERS, 24, STATION_NUM);
-    
-    %parpool
-    
-    parfor iter = 1:NUM_ITERS
-        e = zeros(24, STATION_NUM);
-        e_optimal = rand(24, STATION_NUM);
+        NUM_PARALLEL_ITERS = 100;
 
-        for t = 1:24
-            e_optimal(t, :, :) = e_optimal(t, :, :) / sum(e_optimal(t, :, :)) * BIKE_NUM;
-        end
+        best_errors = zeros(NUM_PARALLEL_ITERS, 1);
+        best_es = zeros(NUM_PARALLEL_ITERS, 24, STATION_NUM);
 
-        total_error = 0;
+        %parpool
 
-        for t = 1:24
-            for current_station = 1:STATION_NUM
-                for other_station = 1:STATION_NUM
-                    enter = e_optimal(t, other_station) * P(t, other_station, current_station);
-                    exit = e_optimal(t, current_station) * P(t, current_station, other_station);
+        parfor iter = 1:NUM_PARALLEL_ITERS
+            e = zeros(24, STATION_NUM);
+            e_optimal = rand(24, STATION_NUM);
 
-                    e(t+1, current_station) = e_optimal(t, current_station) + enter - exit;
+            for t = 1:24
+                e_optimal(t, :, :) = e_optimal(t, :, :) / sum(e_optimal(t, :, :)) * BIKE_NUM;
+            end
 
-                    next_time = mod(t, 24) + 1;
+            total_error = 0;
 
-                    error = e(next_time, current_station) - e_optimal(next_time, current_station);
-                    total_error = total_error + abs(error);
+            for t = 1:24
+                for current_station = 1:STATION_NUM
+                    for other_station = 1:STATION_NUM
+                        enter = e_optimal(t, other_station) * P(t, other_station, current_station);
+                        exit = e_optimal(t, current_station) * P(t, current_station, other_station);
+
+                        e(t+1, current_station) = e_optimal(t, current_station) + enter - exit;
+
+                        next_time = mod(t, 24) + 1;
+
+                        error = e(next_time, current_station) - e_optimal(next_time, current_station);
+                        total_error = total_error + abs(error);
+                    end
                 end
             end
+
+            best_errors(iter) = total_error
+            best_es(iter, :, :) = e_optimal;
+            %iter
         end
-        
-        best_errors(iter) = total_error
-        best_es(iter, :, :) = e_optimal;
-        iter
+
+        best_found = max(best_errors);
+        if best_found < best_error
+            ix = find(best_errors == best_found);
+            best_e = best_es(ix, :, :);
+            dlmwrite('optimal-allocations.matrix', best_e);
+            best_error = best_found
+            disp 'Found local opt'
+        end
     end
-    
-    best_errors
-    ix = find(best_errors == max(best_errors));
-    best_es;
-    best_e = best_es(ix, :, :);
 end
-
-
-
-% Minimize total error by changing initial allocation, e 
